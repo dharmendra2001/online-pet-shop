3a257aaec95a3e5a6b8724d893b7b60986a41a9a
dharmendra2001 <dharmendra@webkorps.com>
2022-10-04 02:59:57 +0530

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    password: 'users/password',
+    registrations: 'users/registrations'
+  }
+end
