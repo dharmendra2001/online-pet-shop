c22e38b63ada9628094eb319eab8bdddfb0d7cbe
dharmendra2001 <dharmendra@webkorps.com>
2022-10-11 03:08:46 +0530

    add contact_number

--- db/migrate/20221010143900_add_contact.rb
+++ db/migrate/20221010143900_add_contact.rb
@@ -2,6 +2,7 @@ class AddContact < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :contact_number, :string
     add_column :users, :stripe_customer_id, :string
+    add_column :users, :membership, :integer, default: 0
     #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
   end
 end
