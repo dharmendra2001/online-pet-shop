524036c0bfbb40808ace8e40356f2d76f8bac853
dharmendra2001 <dharmendra@webkorps.com>
2022-10-12 03:29:29 +0530

    create charge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,14 +15,17 @@ class HomeController < ApplicationController
   end
 
   def charge
+    pet_name = Pet.find_by(id: params[:pet_id]).petname
     check = true
     if check
       s = StripeService.new
       token = s.create_card_token(params)
       customer = s.find_or_create_customer(current_user)
       token= s.create_card_token(params)
-      s.create_stripe_customer_card(token.id,customer)
+      card=s.create_stripe_customer_card(token.id,customer)
+      s.create_stripe_charge(params[:amount_to_be_paid], customer.id, card.id, pet_name)
       current_user.change_user_to_paid
+      flash[:notice] = "Your card has been paid!"
       redirect_to root_path(current_user.id)
     else
       redirect_to user_path(current_user.id), alert: "some error occured"
