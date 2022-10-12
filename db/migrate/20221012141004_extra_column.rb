9e048eef52060ebc9db98e40d669a62c029be947
dharmendra2001 <dharmendra@webkorps.com>
2022-10-12 20:47:00 +0530

    add column price, status, pet_type

---
+++ db/migrate/20221012141004_extra_column.rb
@@ -0,0 +1,8 @@
+class ExtraColumn < ActiveRecord::Migration[7.0]
+  def change
+    add_column :pets, :price, :integer
+    add_column :pets, :status, :integer, :default =>0
+    add_column :pets, :pet_type, :string
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
