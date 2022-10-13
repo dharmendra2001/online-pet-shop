d8c64f5757858ecc3f092b516dc057633b127fbf
dharmendra2001 <dharmendra@webkorps.com>
2022-10-14 03:54:41 +0530

    rescue stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   rescue_from Stripe::APIConnectionError do
-    flash[:alert] = 'No internet connection detected'    
+    flash[:alert] = 'No internet connection available. Please try again later.'     
+    redirect_to root_path
+  end
+
+  rescue_from Stripe::InvalidRequestError do
+    flash[:alert] = 'Could not find payment information'    
     redirect_to root_path
   end
 end
