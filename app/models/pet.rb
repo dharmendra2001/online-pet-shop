b1272d12f20a7db282c2f794d3e7c2b149f1a406
dharmendra2001 <dharmendra@webkorps.com>
2022-10-15 01:05:57 +0530

    add validation

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,7 @@
 class Pet < ApplicationRecord
   belongs_to :petshop 
   has_one_attached :image 
+  has_one_attached :medical
   has_many :buys
+  validates :petname, :food, :pet_type, :weight, :height, :description, :precaution, :image, :medical, :price, presence: true 
 end
