d2028808f3913807ebc536a9407b49a9357e76fd
dharmendra2001 <dharmendra@webkorps.com>
2022-10-15 01:22:37 +0530

    create a route

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   post '/pets/new', to: 'pets#create'
   get 'payment/:pet_id/:price', to: "home#payment", as: "payment"
   post 'charge', to: "home#charge", as: "charge"
-  get 'buy_pet', to: "home#buy_pet", as: "buy_pet"  
+  get 'buy_pet', to: "home#buy_pet", as: "buy_pet" 
+  get 'medical_certificate/:id', to: "pets#medical_certificate", as: "medical_certificate"
+  delete 'destroy_method/:id', to: 'petshops#destroy_method', as: "destroy_method"
 
 end
